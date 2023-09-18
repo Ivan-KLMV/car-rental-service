f18431854cad7d8a15c0e5ba553480ec6d2d4e70
Ivan Klimov <klimov128@gmail.com>
2023-09-19 01:21:08 +0300

    Update SearchForm.jsx

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,94 +1,39 @@
+import Select from 'react-dropdown-select';
+import { v4 as uuidv4 } from 'uuid';
 import styled from 'styled-components';
+import { useState } from 'react';
+import { DatalistInput } from 'components/DatalistInput/DatalistInput';
+import { DatalistInputTwo } from 'components/DatalistInput/DatalistInputTwo';
 
 export const SearchForm = props => {
+  const [selectedCar, setSelectedCar] = useState([]);
+  const [selectedPrice, setSelectedPrice] = useState([]);
+
+  const handleChangeCar = values => {
+    console.log('in form', values);
+    setSelectedCar(values);
+  };
+
+  const handleChangePrice = values => {
+    setSelectedPrice(values);
+  };
   return (
     <form style={{ display: 'flex', gap: 18 }}>
-      <label
-        htmlFor="makes"
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 8,
-
-          fontSize: 14,
-          fontWeight: 500,
-          lineHeight: 1.29,
-          color: '#8A8A89',
-        }}
-      >
-        Car brand
-        <InputStyled
-          list="makes_of_cars"
-          name="makes"
-          id="makes"
-          placeholder="Enter the text"
-        />
-        <DataListStyled id="makes_of_cars">
-          {props.makes.map(make => (
-            <option key={make}>{make}</option>
-          ))}
-        </DataListStyled>
-      </label>
-
-      <label
-        htmlFor="price"
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 8,
-
-          fontSize: 14,
-          fontWeight: 500,
-          lineHeight: 1.29,
-          color: '#8A8A89',
-        }}
-      >
-        Price/ 1 hour
-        <input
-          list="price per hour"
-          name="price"
-          id="price"
-          style={{ padding: '14px 18px' }}
-        />
-        <datalist id="price per hour">
-          {props.rentalPrices.map(item => (
-            <option key={item.id}>{item.rentalPrice}</option>
-          ))}
-        </datalist>
-      </label>
-      <label
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 8,
-
-          fontSize: 14,
-          fontWeight: 500,
-          lineHeight: 1.29,
-          color: '#8A8A89',
-        }}
-      >
-        Сar mileage / km
-        <input />
-        <input />
-      </label>
+      <DatalistInputTwo />
+      <DatalistInput
+        options={props.makes}
+        labelName="Car brand"
+        selectedItems={selectedCar}
+        placeholderText="Enter the text"
+        handleChange={handleChangeCar}
+      />
+      <DatalistInput
+        options={props.rentalPrices}
+        labelName="Price/ 1 hour"
+        selectedItems={selectedPrice}
+        placeholderText="To $"
+        handleChange={handleChangePrice}
+      />
     </form>
   );
 };
-
-const InputStyled = styled.input`
-  padding: 14px 18px;
-  border-radius: 14px;
-  border: none;
-  background-color: #f7f7fb;
-
-  &::placeholder {
-    font-size: 18px;
-    font-weight: 500;
-    opacity: 1;
-    color: #121417;
-  }
-`;
-const DataListStyled = styled.datalist`
-  background-color: lightgreen;
-`;
