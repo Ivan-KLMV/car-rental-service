8587dd6ecabe5364ff71425bf8490676b5263450
Ivan Klimov <klimov128@gmail.com>
2023-09-19 23:00:33 +0300

    Create FavoriteButtonNormal.jsx

---
+++ src/components/Buttons/FavoriteButtonNormal.jsx
@@ -0,0 +1,25 @@
+import { HeartActive, HeartNormal } from 'components/Svg/Svg';
+import { useGetCarsByIdQuery } from 'redux/carsSlice';
+import styled from 'styled-components';
+
+export const FavoriteButtonNormal = ({ isActive, handleClick }) => {
+  //   const { data } = useGetCarsByIdQuery(handleClick, { skip: handleClick });
+  //   console.log(data);
+  return (
+    <FavButtonStyled type="button" onClick={handleClick}>
+      {isActive ? <HeartActive /> : <HeartNormal />}
+    </FavButtonStyled>
+  );
+};
+
+const FavButtonStyled = styled.button`
+  width: 18px;
+  height: 18px;
+  position: absolute;
+  top: 14px;
+  right: 14px;
+  margin: 0;
+  padding: 0;
+  border: unset;
+  background: unset;
+`;
