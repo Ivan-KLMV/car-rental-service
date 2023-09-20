72e652ca9301ed51d95ecc68b3ca39cb6928866e
Ivan Klimov <klimov128@gmail.com>
2023-09-20 22:04:09 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { carsApi } from './carsSlice';
-import { createAction, createReducer } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {
